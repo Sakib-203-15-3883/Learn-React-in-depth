84ac2bb0636d8ba7f4e1363dc8bd20e35494c2dd
Sakib-203-15-3883 <sakibul15.3883@gmail.com>
2023-12-28 12:24:30 +0600

    refactor

--- src/app/to_do_app_ui/page.jsx
+++ src/app/to_do_app_ui/page.jsx
@@ -7,6 +7,9 @@ const ToDoApp = () => {
   return (
     <>
       <center className="m-20 text-6xl font-bold ">To Do App</center>
+      <h1 className="m-20 text-2xl font-bold ">I try to create this dummy ui with reusable components with modularity , consistancy , efficiency and maintainability . <br /> <br />
+
+        here , I create 3 components as button , input_task & to display task data , i create task_data component . At last , i bringing everything together at root component which is this page [To Do App]     </h1>
 
       <div>
         <ul className=" grid grid-cols-5 gap-4 m-10 p-10 bg-blue-300 h-auto ">
