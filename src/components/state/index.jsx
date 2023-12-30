cbedacfb332f58f0c1ee533466646ae475e2394c
Sakib-203-15-3883 <sakibul15.3883@gmail.com>
2023-12-31 00:18:52 +0600

    error fixed

--- src/components/state/index.jsx
+++ src/components/state/index.jsx
@@ -3,9 +3,9 @@ const State = () => {
   return (
     <div>
       <ul className="m-10 p-2">
-        {state.map((item) => {
+        {state.map((item, index) => {
           return (
-            <li className="text-xl font-bold p-2 border-4 m-5 rounded-xl bg-whiteSmoke">
+            <li className="text-xl font-bold p-2 border-4 m-5 rounded-xl bg-whiteSmoke" key={index}>
               <p> # {item} </p>
             </li>
           );
